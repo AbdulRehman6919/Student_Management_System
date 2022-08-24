309af7cd7fed33d6e0d2f079bcb874bf09b11675
AbdulRehman6919 <abdulrehmansamsung6919@gmail.com>
2022-08-24 21:15:46 +0500

    'all_done'

--- Student_Managmenet_System/students/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Student,StudentFees,Teacher, TeacherSalary
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(StudentFees)
-admin.site.register(Teacher)
-admin.site.register(TeacherSalary)
